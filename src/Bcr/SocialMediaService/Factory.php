7ebb5f6c418853f73599dc085a25b9970d1f2df5
Tobias Olry <tobias.olry@gmail.com>
2019-06-18 15:01:36 +0200

    really share instance of client 😁

--- src/Bcr/SocialMediaService/Factory.php
+++ src/Bcr/SocialMediaService/Factory.php
@@ -9,23 +9,28 @@ use Symfony\Component\HttpClient\CurlHttpClient;
 
 final class Factory
 {
+    private $httpClient;
+
+    public function __construct()
+    {
+        $this->httpClient = new CurlHttpClient();
+    }
+
     public function create(Type $type, array $options) : SocialMediaServiceInterface
     {
         // @todo validate options array, specific to type
 
-        $httpClient = new CurlHttpClient();
-
         switch ($type) {
             case Type::TWITTER:
-                return new Twitter($httpClient, $options['username'], $options['key'], $options['secret']);
+                return new Twitter($this->httpClient, $options['username'], $options['key'], $options['secret']);
             case Type::YOUTUBE:
-                return new YouTube($httpClient, $options['api_key'], $options['client_id'], $options['client_secret'], $options['channel_id']);
+                return new YouTube($this->httpClient, $options['api_key'], $options['client_id'], $options['client_secret'], $options['channel_id']);
             case Type::FLICKR:
-                return new Flickr($httpClient, $options['user_id']);
+                return new Flickr($this->httpClient, $options['user_id']);
             case Type::INSTAGRAM:
-                return new Instagram($httpClient, $options['token']);
+                return new Instagram($this->httpClient, $options['token']);
             case Type::RSS:
-                return new Rss($httpClient, $options['feed_url']);
+                return new Rss($this->httpClient, $options['feed_url']);
             default:
                 throw new RuntimeException('unsupported type: ' . (string) $type);
         }
